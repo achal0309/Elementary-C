4ee0054551e61dc0021c074e2aa095c12441d1a5
Achal Raghav <59573057+achal0309@users.noreply.github.com>
2020-01-08 23:03:38 +0530

    Add files via upload
    The Programme provides linear searching in a linear array.

---
+++ Application_of_functions/Array_linear_searchusing_f.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int x,flag=0,position=0,arr[100],size,search;     //Global Variables
+void linear_search(int arr_copy[],int size,int search)
+{
+	for(x=0;x<size;x++)
+ {
+	if(arr_copy[x]==search)
+   {
+	flag=1;
+	position=x;
+	break;
+   }
+	else
+	flag=0;
+ }
+    if(flag==1)
+    printf("\nTHE ELEMENT IS AT POSITION %d\n",position+1);
+    else if(flag==0)
+    printf("\nTHE ARRAY DOESN'T CONTAIN THE GIVEN ELEMENT\n");
+}
+void input()
+{
+	printf("\nENTER THE SIZE OF ARRAY\n");
+	scanf("%d",&size);
+	printf("\n ENTER THE ELEMENTS OF ARRAY\n");
+	for(x=0;x<size;x++)
+	scanf("%d",&arr[x]);
+	printf("\nENTER THE ELEMENT TO BE SEARCHED\n");
+	scanf("%d",&search);	
+}
+int main()                //main body
+{   
+	input();
+	linear_search(arr,size,search);
+	return 0;
+}
